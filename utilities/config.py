61dacba5592b481fe80a99009f39dd2442373559
pallavi422009 <66233599+pallavi422009@users.noreply.github.com>
2024-01-17 21:58:01 +0530

    Update config.py

--- utilities/config.py
+++ utilities/config.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 
-def config(filename='../testData/database.ini', section='postgresql'):
+def config(filename='./testData/database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
